8554e5be9d04767c2fe81cbf701f6ebd927eb434
Hidayadie <hissats234@gmail.com>
2025-08-28 23:57:42 +0700

    add autorole setting 2

--- src/command/command_set_autorole.cpp
+++ src/command/command_set_autorole.cpp
@@ -10,23 +10,30 @@ void Commands::command_set_autorole(dpp::cluster& bot, const dpp::slashcommand_t
 
 
     std::cout << "[DEBUG] Getting role value" << std::endl;
-    dpp::role role;
-    if (event.command.resolved.roles.find(std::get<dpp::snowflake>(event.get_parameter("role"))) != event.command.resolved.roles.end()) {
-            role = event.command.resolved.roles.at(std::get<dpp::snowflake>(event.get_parameter("role")));
-    }
-    std::cout << "[DEBUG] role value: "<< role.get_mention() << std::endl;
+    dpp::snowflake role;
+    auto param = event.get_parameter("role");
 
-     if (!autorole) {
+    if (param.valueless_by_exception() == 0) {
+        role = 0;
+    } else {
+        role = std::get<dpp::snowflake>(event.get_parameter("role"));
+    }
+
+    std::cout << "[DEBUG] role value: "<< std::to_string(role) << std::endl;
+
+
+    if (!autorole) {
         event.reply("Autorole berhasil **dinonaktifkan**.");
     }
 
-    else if (autorole && role.id == 0) {
+    else if (autorole && role == 0) {
         event.reply("Anda harus memilih **role** untuk mengaktifkan autorole.");
     }
 
-    else if (autorole && role.id != 0) {
-        event.reply("Autorole berhasil diaktifkan dengan role: <@&" + std::to_string(role.id) + ">.");
+    else if (autorole && role != 0) {
+        event.reply("Autorole berhasil diaktifkan dengan role: <@&" + std::to_string(role) + ">.");
     }
-    event.reply("Womp womp.");
+
+    event.reply("womp womp");
     std::cout << "[DEBUG] End of line" << std::endl;
 }
