f2692cdae2d8e02b3ab2e9535661ac688568bc0f
Hidayadie <hissats234@gmail.com>
2025-09-25 19:07:58 +0700

    Chore: slashcommand declaration refactor

--- src/command/base_commands.h
+++ src/command/base_commands.h
@@ -22,7 +22,7 @@
 class Commands {
 
     public:
-    static void command_status(      const std::time_t& start,
+    static void command_bot(         const std::time_t& start,
                                            dpp::cluster& bot,
                                      const dpp::slashcommand_t& event),
                 command_server_info (      dpp::cluster& bot,
@@ -39,28 +39,50 @@ class Commands {
                                      const dpp::slashcommand_t& event);
 
     static inline std::map<std::string, dpp::slashcommand> commands_list {
-            {"1", dpp::slashcommand("status", "Info bot", 0)},
+            {"1", dpp::slashcommand("bot", "Info bot", 0)
+                .add_option(dpp::command_option(dpp::co_sub_command, "status", "cek status bot"))
+                .add_option(dpp::command_option(dpp::co_sub_command, "invite", "inpit banh"))
+                .add_option(dpp::command_option(dpp::co_sub_command, "info", "ingfokan"))
 
-            {"2", dpp::slashcommand("server_info", "Cek status server", 0)},
+            },
+
+            {"2", dpp::slashcommand("server", "Command umum untuk server", 0)
+                .add_option(dpp::command_option(dpp::co_sub_command, "info", "informasi server inih"))
+                .add_option(dpp::command_option(dpp::co_sub_command, "icon", "gambar pp server inih"))
+            },
 
             {"3", dpp::slashcommand("say", "Bot says icixiwir echo", 0)
                 .add_option(dpp::command_option(dpp::co_string, "pesan", "textny..", true))
                 .add_option(dpp::command_option(dpp::co_channel, "channel", "Channel tujuan", true))
             },
 
-            {"4", dpp::slashcommand("set_autorole", "setting autorole server", 0)
-                .add_option(dpp::command_option(dpp::co_boolean, "set", "true/false", true))
-                .add_option(dpp::command_option(dpp::co_role, "role", "pilih role", false))
+            {"4", dpp::slashcommand("autorole", "setting autorole server", 0)
+                .add_option(dpp::command_option(dpp::co_sub_command, "set", "aktifkan autorole")
+                    .add_option(dpp::command_option(dpp::co_role, "role", "pilih role", true))
+                )
+                .add_option(dpp::command_option(dpp::co_sub_command, "edit", "edit role autorole")
+                    .add_option(dpp::command_option(dpp::co_role, "role", "pilih role", true))
+                )
+                .add_option(dpp::command_option(dpp::co_sub_command, "disable", "matiin autorole")
+
+                )
                 .set_default_permissions(dpp::p_administrator)
             },
 
-            {"5", dpp::slashcommand("user_info", "lihat profil user", 0)
-                .add_option(dpp::command_option(dpp::co_user, "user", "pilih satu", false))
+            {"5", dpp::slashcommand("user", "command untuk user", 0)
+                .add_option(dpp::command_option(dpp::co_sub_command, "info", "cek info user")
+                    .add_option(dpp::command_option(dpp::co_user, "user", "pilih", true))
+
+                )
+                .add_option(dpp::command_option(dpp::co_sub_command, "avatar", "cek pp user")
+                    .add_option(dpp::command_option(dpp::co_user, "user", "pilih", true))
+
+                )
             },
-            // census
-            {"6", dpp::slashcommand("set_member_count", "setting channel jumlah member", 0)
+
+            {"6", dpp::slashcommand("sensus", "setting channel sensus jumlah member", 0)
                 .add_option(
-                    dpp::command_option(dpp::co_sub_command, "add", "aktifkan member count")
+                    dpp::command_option(dpp::co_sub_command, "add", "aktifkan sensus count")
                         .add_option(
                             dpp::command_option(dpp::co_string, "mode", "jenis member yang dihitung", true)
                                 .add_choice(dpp::command_option_choice("semua members", "members"))
@@ -74,7 +96,7 @@ class Commands {
                 )
 
                 .add_option(
-                    dpp::command_option(dpp::co_sub_command, "remove", "nonaktifkan fitur member count")
+                    dpp::command_option(dpp::co_sub_command, "remove", "nonaktifkan fitur sensus count")
                         .add_option(
                             dpp::command_option(dpp::co_string, "mode", "jenis channel yg mw dihapus", true)
                                 .add_choice(dpp::command_option_choice("all members", "members"))
@@ -84,7 +106,7 @@ class Commands {
                         )
                 )
                 .add_option(
-                    dpp::command_option(dpp::co_sub_command, "edit", "edit namany")
+                    dpp::command_option(dpp::co_sub_command, "edit", "edit nama channel")
                         .add_option(
                             dpp::command_option(dpp::co_string, "mode", "jenis member yang dihitung", true)
                                 .add_choice(dpp::command_option_choice("semua members", "members"))
@@ -100,7 +122,10 @@ class Commands {
                 .set_default_permissions(dpp::p_administrator)
             },
 
-            {"7", dpp::slashcommand("sync_guild", "sinkron config/setting guild", 0)
+            {"7", dpp::slashcommand("guild", "command untuk guild khusus atmint", 0)
+                .add_option(
+                    dpp::command_option(dpp::co_sub_command, "sync", "sinkronisasi database dengan pusat")
+                )
                 .set_default_permissions(dpp::p_administrator)
             }
 
