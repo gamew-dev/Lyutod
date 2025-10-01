794331cff3871ee52d3afbbcd217779c58073a05
Hidayadie <hissats234@gmail.com>
2025-10-01 21:35:45 +0700

    Chore: refactor slashcommand guild

--- src/command/command_guild.cpp
+++ src/command/command_guild.cpp
@@ -1,10 +1,27 @@
 #include "base_commands.h"
 
-void Commands::command_sync_guild(dpp::cluster& bot, const dpp::slashcommand_t& event) {
-    std::string guild_id = std::to_string(event.command.guild_id);
-    std::string channel_id = std::to_string(event.command.channel_id);
+void Commands::command_guild(dpp::cluster& bot, const dpp::slashcommand_t& event) {
 
-    event.reply("membuka database server/guild...");
+    dpp::command_interaction cmd_data = event.command.get_command_interaction();
 
-    Config::syncGuildConfig(bot, guild_id, channel_id);
+    /// Check
+    if (cmd_data.options.empty()) {
+        event.edit_response("pilih subcommandny");
+        return;
+    }
+
+    /// getter sub command option (set, edit, remove)
+    //  and global variable for guild config, id etc
+    auto subcommand = cmd_data.options[0];
+
+    if (subcommand.name == "sync") {
+        std::string guild_id = std::to_string(event.command.guild_id);
+        std::string channel_id = std::to_string(event.command.channel_id);
+
+        event.reply("membuka database server/guild...");
+
+        Config::syncGuildConfig(bot, guild_id, channel_id);
+    }
+
+
 }
